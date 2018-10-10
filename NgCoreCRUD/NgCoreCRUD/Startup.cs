ae2adbe158107e49a4c8d4e3550d44f23807af59
Dmitry <yurasovdv@gmail.com>
2018-10-10 20:37:04 +0400

    now default route is angular app

--- NgCoreCRUD/NgCoreCRUD/Startup.cs
+++ NgCoreCRUD/NgCoreCRUD/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -28,6 +29,13 @@ namespace NgCoreCRUD
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<GalleryDbContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("GalleryDB"), options => options.CommandTimeout(120)));
+
+
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "Frontend/gallery-front/dist";
+            });
+
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -37,7 +45,22 @@ namespace NgCoreCRUD
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+
             app.UseMvc();
+
+            app.UseSpa(builder => 
+            {
+                builder.Options.SourcePath = "Frontend/gallery-front";
+
+
+                if (env.IsDevelopment())
+                {
+                    builder.UseAngularCliServer(npmScript: "start");
+                }
+
+            });
         }
     }
 }
